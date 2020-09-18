88522cd47abe764a1dc5c98e6b82f4237e86a42a
laonstory <laonstory@laonstory.com>
2020-09-18 14:36:47 +0900

    스플래쉬 디자인 수정

--- lib/src/screens/splash.dart
+++ lib/src/screens/splash.dart
@@ -15,18 +15,32 @@ class _Splash extends State<Splash> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      backgroundColor: white,
+      backgroundColor: Colors.yellow,
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/icon/splash.png",
-              width: 56,
-              fit: BoxFit.contain,
+        child: Stack(
+          children: <Widget>[
+            Center(
+//                mainAxisAlignment: MainAxisAlignment.center,
+//                crossAxisAlignment: CrossAxisAlignment.center,
+              child: Image.asset(
+                "assets/icon/splash.png",
+                height: 150,
+                width: 200,
+                fit: BoxFit.contain,
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Image.asset(
+                "assets/icon/dog.png",
+                alignment: Alignment.bottomRight,
+                height: 200,
+                width: 250,
+                fit: BoxFit.contain,
+              ),
             )
           ],
         ),
