c6bf0c8c53e30b6fba1b4afafb9d000294030dfe
laonstory <laonstory@laonstory.com>
2020-10-15 18:37:12 +0900

    공유하기

--- lib/src/screens/center/appinfomation.dart
+++ lib/src/screens/center/appinfomation.dart
@@ -4,6 +4,7 @@ import 'package:cashcook/src/utils/colors.dart';
 import 'package:cashcook/src/widgets/whitespace.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:cashcook/src/utils/Share.dart';
 
 class AppInfomation extends StatelessWidget {
   ScrollController _scrollController = ScrollController();
@@ -53,7 +54,7 @@ class AppInfomation extends StatelessWidget {
               padding: const EdgeInsets.only(right: 16.0),
               child: InkWell(
                 onTap: (){
-                  //Navigator.of(context).pushNamed("/inquiry/write"); //공유 버튼을 눌렀을때 이동할 주소 (임시)
+                  onKakaoShare();
                 },
                 child: Text("공유",style: TextStyle(fontSize: 14,decoration: TextDecoration.underline),), //오른쪽 상단에 텍스트 출력
               ),
@@ -70,19 +71,13 @@ class AppInfomation extends StatelessWidget {
                 ),
                 ClipRRect( // 사각형 이미지를 둥글게하는 함수
                   borderRadius: BorderRadius.circular(15),
-                  child: SizedBox(
+                  child:
+                  SizedBox(
                     width: 100,
                     height: 100,
-                    child: Image.asset('assets/icon/splash.png', fit: BoxFit.cover,),
+                    child: Image.asset('assets/icon/mini_logo.png', fit: BoxFit.cover,),
                   ),
                 ),
-                /*이미지 가져오기
-                Image.asset(
-                  'assets/icon/splash.png',
-                  width: 100,
-                  height: 100,
-                ), //임시
-                */
                 Padding(
                   padding: EdgeInsets.only(bottom: 20),
                 ),
@@ -92,111 +87,4 @@ class AppInfomation extends StatelessWidget {
             )
         ));
   }
-
-//
-//   Widget body() {
-//     return AppinfoList();
-//   }
-//   Widget AppinfoList() {
-//     return Consumer<CenterProvider>(
-//       builder: (context, centers, _){
-//         return ListView.builder(
-//           controller: _scrollController,
-//           itemBuilder: (context, idx) {
-//             if(idx < centers.faq.length){
-//               return AppinfoItem(centers.faq[idx]);
-//             }
-//             return Center(
-//               child: Opacity(
-//                 opacity: centers.isLoading ? 1.0 : 0.0,
-//                 child: CircularProgressIndicator(),
-//               ),
-//             );
-//           },
-//           physics: AlwaysScrollableScrollPhysics(),
-//           itemCount: centers.faq.length + 1,
-//         );
-//       },
-//     );
-//   }
-// }
-//
-// class FaqItem extends StatefulWidget {
-//   FaqModel faqModel;
-//   FaqItem(this.faqModel);
-//
-//   @override
-//   _FaqItemState createState() => _FaqItemState();
-// }
-//
-// class _FaqItemState extends State<FaqItem> {
-//   bool isOpen = false;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       child: Column(
-//         children: [
-//           InkWell(
-//               onTap: () {
-//                 setState(() {
-//                   isOpen = !isOpen;
-//                 });
-//               },
-//               child: title()),
-//           isOpen ? detail() : SizedBox(),
-//         ],
-//       ),
-//     );
-//   }
-//
-//   Widget title() {
-//     return Container(
-//       width: double.infinity,
-//       height: 64,
-//       padding: const EdgeInsets.symmetric(horizontal: 16),
-//       child: Align(
-//         alignment: Alignment.centerLeft,
-//         child: Row(
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: [
-//             Image.asset("assets/icon/q.png",fit: BoxFit.contain, width: 24,),
-//             whiteSpaceW(12),
-//             Text(
-//               widget.faqModel.question,
-//               style: TextStyle(
-//                 fontSize: 14,
-//                 fontWeight: FontWeight.w600,
-//                 color: Color(0xff444444),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-//
-//   Widget detail() {
-//     return Container(
-//       width: double.infinity,
-//       color: Color(0xfff7f7f7),
-//       child: Padding(
-//         padding:
-//         const EdgeInsets.only(left: 16, right: 16, top: 12, bottom: 24),
-//         child: Row(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             Image.asset("assets/icon/a.png",fit: BoxFit.contain, width: 24,),
-//             whiteSpaceW(12),
-//             Text(
-//               widget.faqModel.answer,
-//               style: TextStyle(fontSize: 12, color: Color(0xff444444)),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-//
-
-
 }
